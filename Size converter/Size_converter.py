702f0b9be5e5f6c0dc41c447ccc9a1ab3f2898e2
AndyKor666 <andrey.korotkov1304@gmail.com>
2025-10-23 23:12:43 +0300

    Printing the main console

---
+++ Size converter/Size_converter.py
@@ -0,0 +1,8 @@
+print("=========== VOLUME CONVERTER SYSTEM ===========")
+print("-----------------------------------------------")
+print(">> 1 - Convert liters -> milliliters.")
+print(">> 2 - Convert milliliters -> liters.")
+print(">> 3 - Convert liters -> cubic meters.")
+print(">> 4 - Convert cubic meters -> liters.")
+print(">> 0 - EXIT.")
+print("-----------------------------------------------")
