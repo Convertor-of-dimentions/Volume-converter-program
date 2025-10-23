f8dab56ee559b5ff8e11d812a8957f0af7387083
AndyKor666 <andrey.korotkov1304@gmail.com>
2025-10-23 23:01:17 +0300

    Function for main logic code

--- Size converter/Size_converter.py
+++ Size converter/Size_converter.py
@@ -9,3 +9,33 @@ def liters_to_cubic_meters(liters):
 
 def cubic_meters_to_liters(m3):
     return m3 * 1000
+
+def main():
+  
+    while True:
+
+        if not choice.isdigit() or int(choice) not in range(0, 5):
+           
+            continue
+
+        choice = int(choice)
+        if choice == 0:
+
+            break
+        try:
+
+            if value <= 0:
+               
+                continue
+
+        except ValueError:
+          
+            continue
+
+        if choice == 1:
+          
+        elif choice == 2:
+          
+        elif choice == 3:
+            
+        elif choice == 4:
