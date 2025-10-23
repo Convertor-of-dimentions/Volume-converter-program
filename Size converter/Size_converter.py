c906286f06fac5f67be6f03b6093c734813e87c7
AndyKor666 <andrey.korotkov1304@gmail.com>
2025-10-23 23:36:12 +0300

    Printing different calculations

--- Size converter/Size_converter.py
+++ Size converter/Size_converter.py
@@ -14,6 +14,14 @@ choice = input(">> Choose an option (0-4): ").strip()
 print(">> ERR . . . Value must be positive and non-zero.")
 print(">> ERR . . . Please enter only a number.")
 print(">> ERR . . . Invalid option. Try again.")
+ 
 
-
-    
+print("-----------------------------------------------")
+print(f">> {value} liters = {liters_to_milliliters(value):.2f} milliliters.")
+print("-----------------------------------------------")
+print(f">> {value} milliliters = {milliliters_to_liters(value):.3f} liters.")
+print("-----------------------------------------------")
+print(f">> {value} liters = {liters_to_cubic_meters(value):.3f} cubic meters.")
+print("-----------------------------------------------")
+print(f">> {value} cubic meters = {cubic_meters_to_liters(value):.1f} liters.")
+print("-----------------------------------------------")
